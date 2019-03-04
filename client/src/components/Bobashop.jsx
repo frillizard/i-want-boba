5f8e4f71807b7b95e99a0ce00439345931534794
Rikki Sasda <rikkisasda@gmail.com>
2019-03-04 04:23:51 -0600

    added rating stars

--- client/src/components/Bobashop.jsx
+++ client/src/components/Bobashop.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+const halfCheck = (rating) => {
+  return rating % 1 > 0 ? `rating-${Math.floor(rating)}-half` : `rating-${rating}`;
+}
+
 const Boba = ({shop}) => {
     return (
       <div className='list-item'>
@@ -7,9 +11,19 @@ const Boba = ({shop}) => {
           <div><img src={shop.image_url} className="list-item-image"/></div>
           <div className='list-item-container'>
             <div className='list-item-title'>{shop.name}</div>
-            <div>{shop.rating}</div>
-            <div>Based on {shop.review_count} reviews</div>
-            <a href={shop.url}>View on Yelp</a>
+            <div className='rating-container'>
+              <img className={`${halfCheck(shop.rating)}`}/>
+              {shop.review_count} reviews
+            </div>
+            <div>{`${shop.location.city}, ${shop.location.state}`}</div>
+            <div>
+              <a href={shop.url} target='_blank'>More info on Yelp</a>
+            </div>
+            <div>
+              <a href={`https://www.google.com/maps/search/?api=1&query=${shop.coordinates.latitude}, ${shop.coordinates.longitude}`} target='_blank'>
+                Open in Google Maps
+              </a>
+            </div>
           </div>
         </div>
       </div>
